a54b7139faba30e701c5a6162f5cd181516ded07
JulienAssouline <julien1993@hotmail.ca>
2019-05-28 11:03:44 -0400

    Changed cookie to be not secure

--- server/resolvers/mutation/setCookie.js
+++ server/resolvers/mutation/setCookie.js
@@ -1,7 +1,7 @@
 const setCookie = ({ tokenName, token, res }) => {
   res.cookie(tokenName, token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
   })
 }
 
